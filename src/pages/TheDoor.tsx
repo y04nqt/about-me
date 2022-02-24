feb8c1187dade4bf704bd00bffe7dc7d9de2e739
Aaron Krueger <aaron@moonrisers.com>
2022-02-23 18:42:57 -0600

    flip between

--- src/pages/TheDoor.tsx
+++ src/pages/TheDoor.tsx
@@ -5,24 +5,23 @@ const TIME = 500000;
 export default function TheDoor(){
   const [open, setOpen] = useState(false);
   const [progress, setProgress] = useState(0);
+
   useEffect(() => {
-    if (!open) {
-      let timer = window.setTimeout(() => {
-        setOpen(true);
-      }, TIME);
+    let timer = window.setTimeout(() => {
+      setOpen(!open);
+    }, TIME);
 
-      let ticker = TIME;
-      let interval = window.setInterval(() => {
-        ticker -= 1000;
-        const prog = Math.abs((ticker/TIME) - 1) * 100;
-        setProgress(Number(prog.toFixed(2)));
-      }, 1000);
+    let ticker = TIME;
+    let interval = window.setInterval(() => {
+      ticker -= 1000;
+      const prog = Math.abs((ticker/TIME) - 1) * 100;
+      setProgress(Number(prog.toFixed(2)));
+    }, 1000);
 
-      return () => {
-        clearTimeout(timer);
-        clearInterval(interval);
-      };
-    }
+    return () => {
+      clearTimeout(timer);
+      clearInterval(interval);
+    };
   }, [open]);
 
   return(
@@ -46,49 +45,18 @@ export default function TheDoor(){
         >
           Here's the beginning of your journey into the truth.
         </h2>
-        {!open && (
-          <Frame
-            bg="aliceblue"
-            width="250px"
-            height="250px"
-            className='p-4 rounded-full mx-auto absolute'
-            style={{
-              top: 'calc(50% - 125px)',
-              left: 'calc(50% - 125px)',
-            }}
-            boxShadow="in"
-          >
-            <div
-              id="pikachu"
-              className='circles mx-auto rounded-full flex align-middle justify-center'
-            >
-              <h1
-                className='self-center rounded-lg'
-              >
-                Thank you<br/>for<br/>stopping by!
-              </h1>
-            </div>
-            <section
-              className={`mt-8 mx-auto text-lg py-8 bg-color-green text-center flex justify-center font-bold`}
-            >
-              <ProgressBar
-                width={window.innerWidth/2}
-                percent={progress}
-              />
-            </section>
-          </Frame>
-        )}
-        {open && (
-          <Frame
-            bg="#F7F7F7"
-            width="250px"
-            height="250px"
-            className='p-4 rounded-full mx-auto absolute'
-            style={{
-              top: 'calc(50% - 125px)',
-              left: 'calc(50% - 125px)',
-            }}
-          >
+        <Frame
+          bg={open ? "aliceblue" : "#F7F7F7"}
+          width="250px"
+          height="250px"
+          className='p-4 rounded-full mx-auto absolute'
+          style={{
+            top: 'calc(50% - 125px)',
+            left: 'calc(50% - 125px)',
+          }}
+          boxShadow="in"
+        >
+          {open ? (
             <div
               id="doge"
               className='circles mx-auto rounded-full flex align-middle justify-center'
@@ -99,8 +67,27 @@ export default function TheDoor(){
                 My friend hates timers so I made one to spite him!
               </h1>
             </div>
-          </Frame>
-        )}
+          ) : (
+            <div
+              id="pikachu"
+              className='circles mx-auto rounded-full flex align-middle justify-center'
+            >
+              <h1
+                className='self-center rounded-lg'
+              >
+                Thank you<br/>for<br/>stopping by!
+              </h1>
+            </div>
+          )}
+            <section
+              className={`mt-8 mx-auto text-lg py-8 bg-color-green text-center flex justify-center font-bold`}
+            >
+              <ProgressBar
+                width={window.innerWidth/2}
+                percent={progress}
+              />
+            </section>
+        </Frame>
       </Frame>
     </Frame>
   )
